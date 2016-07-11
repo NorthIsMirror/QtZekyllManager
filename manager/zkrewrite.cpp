c3e4ea8a6921ca27e3c9356e301466a72beb8863
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-07-11 14:07:43 +0200

    ZkRewrite

---
+++ manager/zkrewrite.cpp
@@ -0,0 +1,28 @@
+#include "zkrewrite.h"
+#include <QDebug>
+
+ZkRewrite::ZkRewrite(QObject *parent) : QObject(parent)
+{
+    QObject::connect(&process_,
+                     SIGNAL(finished(int, QProcess::ExitStatus)),
+                     this,
+                     SLOT(handleZkRewrite(int, QProcess::ExitStatus)));
+}
+
+void ZkRewrite::handleZkRewrite(int exitCode, QProcess::ExitStatus exitStatus) {
+    qDebug() << exitCode;
+    qDebug() << exitStatus;
+    QString buffer = static_cast<QIODevice*>(QObject::sender())->readAll();
+    QStringList entries = buffer.split("\n", QString::SkipEmptyParts);
+
+    emit result(exitCode, entries);
+}
+
+void ZkRewrite::rewrite() {
+    arguments_.clear();
+    arguments_ << "-qn" << "-p" << repoPath_ << "-z" << in_zekylls_ << "-Z" << out_zekylls_;
+    process_.kill();
+    process_.waitForFinished(50);
+    process_.start("zkrewrite", arguments_);
+    process_.waitForStarted();
+}
---
+++ manager/zkrewrite.h
@@ -0,0 +1,32 @@
+#ifndef ZKREWRITE_H
+#define ZKREWRITE_H
+
+#include <QObject>
+#include <QProcess>
+
+class ZkRewrite : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ZkRewrite(QObject *parent = 0);
+    void setInZekylls( const QString & zekylls ) { in_zekylls_ = zekylls; }
+    void setOutZekylls( const QString & zekylls ) { out_zekylls_ = zekylls; }
+    void setRepoPath( const QString & path ) { repoPath_ = path; }
+
+signals:
+    void result( int exitCode, QStringList reply );
+
+public slots:
+
+    void handleZkRewrite( int exitCode, QProcess::ExitStatus exitStatus );
+    void rewrite();
+
+private:
+    QString in_zekylls_;
+    QString out_zekylls_;
+    QString repoPath_;
+    QProcess process_;
+    QStringList arguments_;
+};
+
+#endif // ZKIREWRITE_H
