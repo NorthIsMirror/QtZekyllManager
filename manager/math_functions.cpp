cd589140cd255ba5c485e2636dbcc36e88975cb2
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-07-13 14:57:58 +0200

    letters_to_numbers

--- manager/math_functions.cpp
+++ manager/math_functions.cpp
@@ -1,9 +1,16 @@
 #include "math_functions.h"
 #include <QDebug>
 #include <algorithm>
+#include "singleton.h"
+#include "messages.h"
 
-char characters[36]={ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-                      'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+#define MessagesI Singleton<Messages>::instance()
+
+const char characters[37]={ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                      'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\0' };
+
+const char *characters_begin = &characters[0];
+const char *characters_end = &characters[36];
 
 // FUNCTION: convert_integer_to_base_36 {{{
 // Takes number, returns string [a-z0-9]+ that
@@ -43,11 +50,39 @@ std::tuple< std::vector<char>, int> numbers_to_letters(const std::vector<int> & 
     for( std::vector<int>::const_iterator it = digits.begin(); it != digits.end(); it ++ ) {
         int i = *it;
         if ( i < 0 || i > 35 ) {
-            qDebug() << "Incorrect number during character conversion: " << i;
+            MessagesI.AppendMessageT( "Incorrect number during character conversion: " + QString("%1").arg(i) );
             return std::tuple<std::vector<char>, int>( REPLY, 1 );
         }
         REPLY.push_back( characters[i] );
     }
 
     return std::tuple<std::vector<char>, int>( REPLY, 0 );
+}
+
+// FUNCTION: letters_to_numbers {{{
+// Converts each character of input string into number 0..35
+std::tuple< std::vector<int>, int> letters_to_numbers( const std::vector<char> & letters ) {
+    std::vector<int> reply;
+
+    QString str = QString::fromLatin1( &letters[0], letters.size() );
+    QRegExp rx("^[a-z0-9]+$");
+    rx.setCaseSensitivity( Qt::CaseSensitive );
+    if ( rx.indexIn( str ) == -1 ) {
+        MessagesI.AppendMessageT( "Incorrect character during conversion, allowed are a-z and 0-9" );
+        return make_tuple( reply, -1 );
+    }
+
+    int number;
+    for( std::vector<char>::const_iterator it = letters.begin(); it != letters.end(); it ++ ) {
+        const char *number_it = std::find( characters_begin, characters_end, *it );
+        number = std::distance( characters_begin, number_it );
+        reply.push_back( number );
+    }
+
+    return make_tuple( reply, 0 );
+}
+
+std::tuple< std::vector<int>, int> letters_to_numbers( const std::string & letters ) {
+    std::vector<char> letters2( letters.c_str(), letters.c_str() + letters.size() );
+    return letters_to_numbers( letters2 );
 }
--- manager/math_functions.h
+++ manager/math_functions.h
@@ -3,8 +3,10 @@
 #include <vector>
 #include <tuple>
 
-std::tuple< std::vector<char>, int> convert_integer_to_base_36(int number);
-std::tuple< std::vector<char>, int> numbers_to_letters(const std::vector<int> & digits);
+std::tuple< std::vector<char>, int> convert_integer_to_base_36( int number );
+std::tuple< std::vector<char>, int> numbers_to_letters( const std::vector<int> & digits );
+std::tuple< std::vector<int>, int> letters_to_numbers( const std::vector<char> & letters );
+std::tuple< std::vector<int>, int> letters_to_numbers( const std::string & letters );
 
 
 
