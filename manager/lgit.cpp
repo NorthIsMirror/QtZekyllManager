5dfe903ff497d3c23258d3b0c97cf0f6fc141b9c
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-07-15 14:17:00 +0200

    Extend git stub class

--- manager/lgit.cpp
+++ manager/lgit.cpp
@@ -1,6 +1,21 @@
 #include "lgit.h"
+#ifdef _WIN32
+# include <Windows.h>
+#else
+# include <unistd.h>
+#endif
 
-lgit::lgit(QObject *parent) : QObject(parent)
+lgit::lgit(QObject *parent) : QObject(parent) , repo_(NULL)
+{
+    git_libgit2_init();
+}
+
+lgit::~lgit()
+{
+    git_libgit2_shutdown();
+}
+
+int lgit::openRepo()
 {
 
 }
--- manager/lgit.h
+++ manager/lgit.h
@@ -1,17 +1,28 @@
 #ifndef LGIT_H
 #define LGIT_H
 
+#include <QString>
 #include <QObject>
+
+#include "git2.h"
 
 class lgit : public QObject
 {
     Q_OBJECT
 public:
     explicit lgit(QObject *parent = 0);
+    ~lgit();
+
+    void setRepo( const QString & repo_path ) { repo_path_ = repo_path; }
+    int openRepo();
 
 signals:
 
 public slots:
+
+private:
+    QString repo_path_;
+    git_repository *repo_;
 };
 
 #endif // LGIT_H
