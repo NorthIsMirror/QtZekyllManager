8dc30e0d83cee13124cf4ecb96ad94d90f32dfff
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-08-11 01:03:50 +0200

    Selection on fetchHeadCombo reloads repository state, not only analyses

--- manager/pulldialog.cpp
+++ manager/pulldialog.cpp
@@ -270,13 +270,7 @@ int PullDialog::logOfTip( QString sha, QString hide )
 
 void PullDialog::on_fetchHeadCombo_activated( int index )
 {
-    // List log of selected FETCH_HEAD tip
-    if( ui->fetchHeadCombo->count() > 0 ) {
-        int idx = ui->fetchHeadCombo->currentData().toInt();
-        const mybranch & b = lgit_->branches()[ idx ];
-        logOfTip( QString::fromStdString( b.tip_sha ), QString::fromStdString( b.name ) );
-        updateMergeAnalysis();
-    }
+    reset();
 }
 
 void PullDialog::on_fetchBranch_clicked()
